d1bd58e1a24c5b13588cbb9dcae555176e68c59a
Samyuktha P <samyukthapeeyems@gmail.com>
2022-11-05 10:10:13 +0530

    minor changes

--- src/contexts/Auth.js
+++ src/contexts/Auth.js
@@ -2,14 +2,14 @@ import { createContext, useReducer } from "react";
 import { initialState, AuthReducer } from "../state/AuthReducer";
 
 
-const AuthContext = createContext()
+const AuthContext = createContext(initialState)
 
 
 
 export default function AuthProvider({children}) {
 
     const [state, dispatch] = useReducer(AuthReducer, initialState)
-
+    const signIn = () => {}
 
 
     const values = {
