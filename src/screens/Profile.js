d5a986aa4dfcdd36ae7c76f7f9969eb67a7ca480
Samyuktha P <samyukthapeeyems@gmail.com>
2022-11-14 10:07:39 +0530

    minor changes in auth

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -6,26 +6,44 @@ export default Profile = () => {
         <Button
             title={'Pay with Razorpay'}
             onPress={() => {
-              var options = {
-                description: 'Credits towards consultation',
-              //  image: 'https://i.imgur.com/3g7nmJC.png',
-                currency: 'INR',
-                key: 'rzp_test_63Ql0ORtf7nOeb', // Your api key
-                amount: '5000',
-                method : 'upi'
-                // name: 'foo',
-            
-              };
-              RazorpayCheckout.open(options)
-                .then(data => {
-                  // handle success
-                  alert(`Success: ${data.razorpay_payment_id}`);
-                })
-                .catch(error => {
-                  // handle failure
-                  alert(`Error: ${error.code} | ${error.description}`);
-                });
+                var options = {
+                    description: 'Credits towards consultation',
+                    //  image: 'https://i.imgur.com/3g7nmJC.png',
+                    currency: 'INR',
+                    key: 'rzp_test_63Ql0ORtf7nOeb', // Your api key
+                    amount: '5000',
+                    // name: 'foo',
+                    config: {
+                        display: {
+                            blocks: {
+                                banks: {
+                                    name: 'Pay via UPI',
+                                    instruments: [
+                                        {
+                                            method: 'upi'
+                                        }
+                                    ],
+                                },
+                            },
+                            sequence: ['block.banks'],
+                            preferences: {
+                                show_default_blocks: false,
+                            },
+                        },
+                    },
+
+
+                };
+                RazorpayCheckout.open(options)
+                    .then(data => {
+                        // handle success
+                        alert(`Success: ${data.razorpay_payment_id}`);
+                    })
+                    .catch(error => {
+                        // handle failure
+                        alert(`Error: ${error.code} | ${error.description}`);
+                    });
             }}
-          />
+        />
     )
 }
